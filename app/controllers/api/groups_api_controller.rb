f3c2a1a0ff67e497ef44299aa37d614a48eebb12
Egor Philippov <hoboman313@hotmail.com>
2012-05-09 17:01:03 -0700

    ws

--- app/controllers/api/groups_api_controller.rb
+++ app/controllers/api/groups_api_controller.rb
@@ -5,26 +5,26 @@ module Api
   # Uses Rails' RESTful routes (check 'rake routes' for the configured routes)
   class GroupsApiController < MainApiController
     # TODO: WRITE ME!!!
-  
+
   def create
     render 'shared/http_status', :locals => { :code => "404", :message => HttpStatusHelper::ERROR_CODE["message"]["404"] }, :status => 404
   end
-    
+
   def destroy
     render 'shared/http_status', :locals => { :code => "404", :message => HttpStatusHelper::ERROR_CODE["message"]["404"] }, :status => 404
   end
-  
+
   def update
     render 'shared/http_status', :locals => { :code => "404", :message => HttpStatusHelper::ERROR_CODE["message"]["404"] }, :status => 404
   end
-  
+
   def index
     render 'shared/http_status', :locals => { :code => "404", :message => HttpStatusHelper::ERROR_CODE["message"]["404"] }, :status => 404
   end
-  
+
   def show
     render 'shared/http_status', :locals => { :code => "404", :message => HttpStatusHelper::ERROR_CODE["message"]["404"] }, :status => 404
   end
-    
+
   end # end GroupsApiController
 end
